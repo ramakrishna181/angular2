6f7c0756d62342bd5e6529bbb0cac08db0ec5578
sarada k <sarada k>
2017-05-11 22:34:59 +0530

    as

---
+++ Angular2Demo1/app/contactConsts.ts
@@ -0,0 +1,11 @@
+import {contactRemote} from "./contactRemote";
+/**
+ * Created by DELL PC on 3/17/2017.
+ */
+export const CONTACTS:contactRemote[]=[
+    {firsntName:"Rama",lastName:"Krishna",email:"rama@asdf.com"},
+    {firsntName:"Sarada",lastName:"guthula",email:"saradfa@sdfs.coms"},
+    {firsntName:"Jayant",lastName:"guthula",email:"Jasdfsd@sadf.com"},
+    {firsntName:"asdfasdf",lastName:"eafsdfse",email:"sdfes.sdf"}
+
+    ];
